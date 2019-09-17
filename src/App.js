489b7ae33e56a0ad07a3b9cb64d244e62f5fc361
kor-Don <dy950328@gmail.com>
2019-09-17 21:07:20 +0900

    ee

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
-function App() {
+const GlobalStyles = createGlobalStyle`
+${reset}
+`;
+
+const Container = styled.div`
+  background-color: black;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  div:not(:last-child) {
+    margin-top: 20px;
+  }
+`;
+
+const Clock = styled.div`
+  font-size: 24px;
+`;
+
+const Greeting = styled.div`
+  font-size: 14px;
+`;
+
+const Message = styled.div``;
+
+function App(props) {
+  const [date, setDate] = useState(new Date());
+
+  //Replaces componentDidMount and componentWillUnmount
+  useEffect(() => {
+    var timerID = setInterval(() => tick(), 1000);
+
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
+
+  function tick() {
+    setDate(new Date());
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles></GlobalStyles>
+      <Container>
+        <Clock>{date.toLocaleTimeString()}.</Clock>
+        <Greeting>안녕하세요 97번 좌석입니다.</Greeting>
+        <Message></Message>
+      </Container>
+    </>
   );
 }
 
